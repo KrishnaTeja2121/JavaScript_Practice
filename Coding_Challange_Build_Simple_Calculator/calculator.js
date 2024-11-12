040ae51398f4575c30ac9277fc7c85bdb5f6b5b1
KrishnaTeja2121 <37261298+KrishnaTeja2121@users.noreply.github.com>
2024-11-12 11:40:10 +0530

    Coding Challenge- Simple Calculator

---
+++ Coding_Challange_Build_Simple_Calculator/calculator.js
@@ -0,0 +1,49 @@
+/**
+ * Building a simple Calculator that performs basic operations like addition, Subtraction, multiplication
+ * and division. Use different types of functions, such as Function expressions and Arrow functions for each operation
+ */
+
+
+//addition
+function add(a, b){
+    
+
+}
+console.log(add(10,2));
+
+
+//addition using function expression
+
+let val=function (a,b) {
+    
+
+}
+
+//addition using Arrow Function
+
+const var2 = (a,b) => {
+    let result= a + b;
+    return result;
+}
+
+
+//! Subtraction
+
+const sub_var=(m,n)=>{
+    let varu=m-n;
+    return varu;
+};
+
+//! Multiply
+
+const mukl_var=(m,n)=>{
+    let varu=m*n;
+    return varu;
+};
+
+//! Division
+
+const div_var=(m,n)=>{
+    let varu=m/n;
+    return varu;
+};
