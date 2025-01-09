80e67bf71c58ad83e1ab7b8f639c5dcf0d737fcb
KrishnaTeja2121 <37261298+KrishnaTeja2121@users.noreply.github.com>
2025-01-09 12:25:10 +0530

    updtes#2

---
+++ Creating_HTML_Ele_using_Javascript/script.js
@@ -0,0 +1,20 @@
+//! 1. Select the Target Element
+const appContainer=document.getElementById("app");
+const generateBtn=document.getElementById("generateBtn");
+
+//! 2. Create a new HTML elements (document.createElement())
+function createProfileCard(){
+    //creating the profile card container
+    const profileCard=document.createElement("div");
+    profileCard.className="profile-card";
+}
+//! 3. Add content to elements (innerHTML/textContent)
+//! 4. Add classes or id's if needed // add any attributes
+//! 5. Append the elements to build the structure
+//! Append to the DOM
+
+
+
+
+
+
